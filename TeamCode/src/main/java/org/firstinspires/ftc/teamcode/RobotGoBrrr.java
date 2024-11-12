14ea3f973d7c86553dc778a22fce62a78b6b3700
ecefour15 <95937284+ecefour15@users.noreply.github.com>
2024-11-12 14:24:27 -0500

    erg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotGoBrrr.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotGoBrrr.java
@@ -29,15 +29,21 @@ public class RobotGoBrrr extends LinearOpMode {
 
     // Declare OpMode members.
     private final ElapsedTime runtime = new ElapsedTime();
-    private final ElapsedTime slideTimer = new ElapsedTime();
-    public static ExtensionMode slidestate = ExtensionMode.IDLE;
+//    private final ElapsedTime slideTimer = new ElapsedTime();
+//    public static ExtensionMode slidestate = ExtensionMode.IDLE;
 
     // Create vars
     public static double x = 0;
     public static double y = 0;
     public static double rx = 0;
 
-    public static ExtensionMode vSlideState = ExtensionMode.IDLE;
+    //public static ExtensionMode vSlideState = ExtensionMode.IDLE;
+
+    enum transferState {            // states for scoring and whatnot
+        H_IDLE, V_IDLE, TRANSFERED, H_EXTENDED, H_INTAKEN
+    }
+
+    private transferState currentTransferState = transferState.H_IDLE;
 
     GoBildaPinpointDriver odo;
 
@@ -48,7 +54,7 @@ public class RobotGoBrrr extends LinearOpMode {
         Pose2d beginPose = new Pose2d(0, 0, 0);
 
         GlobalTelemetry.init(telemetry);
-        
+
         GlobalTelemetry.get().addData("Status", "Initialized");
         GlobalTelemetry.get().update();
 
@@ -64,20 +70,20 @@ public class RobotGoBrrr extends LinearOpMode {
         odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
         odo.resetPosAndIMU();
 
-//        DcMotorV2 fLMotor = new DcMotorV2("front_left", hardwareMap);
-//        DcMotorV2 fRMotor = new DcMotorV2( "front_right", hardwareMap);
-//        DcMotorV2 bLMotor = new DcMotorV2("back_left", hardwareMap);
-//        DcMotorV2 bRMotor = new DcMotorV2("back_right", hardwareMap);
+        DcMotorV2 fLMotor = new DcMotorV2("leftFront", hardwareMap);
+        DcMotorV2 fRMotor = new DcMotorV2("rightFront", hardwareMap);
+        DcMotorV2 bLMotor = new DcMotorV2("leftBack", hardwareMap);
+        DcMotorV2 bRMotor = new DcMotorV2("rightBack", hardwareMap);
 
         ServoV2 intakePivotServoOne = new ServoV2("intake_pivot_one", hardwareMap);
         ServoV2 intakePivotServoTwo = new ServoV2("intake_pivot_two", hardwareMap);
 
-       // ServoV2 intakeClawServo = new ServoV2("intake_claw", hardwareMap);
-       // ServoV2 outtakeClawServo = new ServoV2("outtake_claw", hardwareMap);
+        ServoV2 intakeClawServo = new ServoV2("intake_claw", hardwareMap);
+        ServoV2 outtakeClawServo = new ServoV2("outtake_claw", hardwareMap);
 
-        //ServoV2 outtakePivotServo = new ServoV2("outtake_pivot", hardwareMap);
+        ServoV2 outtakePivotServo = new ServoV2("outtake_pivot_one", hardwareMap);
 
-      //  ServoV2 intakeWristServo = new ServoV2("intake_wrist", hardwareMap);
+        ServoV2 intakeWristServo = new ServoV2("intake_wrist", hardwareMap);
 
         DcMotorV2 hSlideMotor = new DcMotorV2("h_slide", hardwareMap);
         hSlideMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -93,56 +99,60 @@ public class RobotGoBrrr extends LinearOpMode {
         vSlideMotorTwo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         vSlideMotorTwo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-//        fLMotor.setDirection(FORWARD);
-//        fRMotor.setDirection(REVERSE);
-//        bLMotor.setDirection(FORWARD);
-//        bRMotor.setDirection(REVERSE);
-//
-//        fLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        fRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        bLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        bRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fLMotor.setDirection(FORWARD);
+        fRMotor.setDirection(REVERSE);
+        bLMotor.setDirection(FORWARD);
+        bRMotor.setDirection(REVERSE);
+
+        fLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         hSlideMotor.setDirection(FORWARD);
 
+
         IMUV2 imu = new IMUV2("imu", hardwareMap);
 
-        /* MechanumDrive mechanumDrive = new MechanumDrive
-                (fLMotor,
-                fRMotor,
-                bLMotor,
-                bRMotor,
-                imu,
-                gamepad1, gamepad2);
-         */
+//         MechanumDrive mechanumDrive = new MechanumDrive
+//                (fLMotor,
+//                fRMotor,
+//                bLMotor,
+//                bRMotor,
+//                imu,
+//                gamepad1, gamepad2);
+
 
         Sequence sequence = new Sequence();
-        
+
         sequence.create("transfer")
-                .add(intakePivotServoOne, .57f, 0) // intake arm servos move to transfer position
-             //   .add(intakePivotServoTwo, .57f, 0)
-                .add(intakeWristServo, 0f, 0)
+                .add(intakePivotServoOne, .59f, 0) // intake arm servos move to transfer position
+                //   .add(intakePivotServoTwo, .57f, 0)
+                .add(intakeWristServo, 0f, 700)
                 .add(hSlideMotor, 0f, 400)
-                .add(outtakeClawServo, 0.5f, 400) //outtake claw grabs sample
-                .add(intakeClawServo, 0.4f,  ) //release sample
-               
+                .add(outtakeClawServo, 0.65f, 400) //outtake claw grabs sample
+                .add(intakeClawServo, 0.4f, 0) //release sample
+                .add(outtakePivotServo, .35f, 0)
+
+
                 //.add(outtakePivotServo, 0.5f, 0) //outtake pivot/claw moves to scoring position
                 .build();
 
         sequence.create("intakeNeutral")
                 //.add(intakeClawServo, 0.5f, 10)
                 .add(hSlideMotor, 200f, 300)
-                .add(intakePivotServoOne, 0f, 0) // intake arm servos move to transfer position
-             //   .add(intakePivotServoTwo, 0f, 0)
+                .add(outtakePivotServo, .7f, 0)
+                .add(intakePivotServoOne, .05, 0) // intake arm servos move to transfer position
+                //   .add(intakePivotServoTwo, 0f, 0)
                 .add(intakeWristServo, .98f, 0)
-                .add(intakeClawServo, .4f, 0 )
+                .add(intakeClawServo, .4f, 0)
                 .build();
 
         sequence.create("intakeGrab")
 
                 .add(intakeClawServo, .9f, 400)
-                .add(intakePivotServoOne, .2f, 0) 
-           //     .add(intakePivotServoTwo, .2f, 0)
+                .add(intakePivotServoOne, .2f, 0)
+                //     .add(intakePivotServoTwo, .2f, 0)
 
 
                 .build();
@@ -172,75 +182,87 @@ public class RobotGoBrrr extends LinearOpMode {
             Pose2D pos = odo.getPosition();
             Pose2D vel = odo.getVelocity();
 
-            if(gamepad1.options) imu.resetYaw();
+            if (gamepad1.options) imu.resetYaw();
 
             x = gamepad1.left_stick_y; // Y is reversed because gamepads are dumb
             y = -gamepad1.left_stick_x;
             rx = -gamepad1.right_stick_x;
 
-            if (gamepad1.a) { // Extend the slide
-                hSlideMotor.runToPosition(280);
-            } else if (gamepad1.b) { // Retract the slide
-                hSlideMotor.runToPosition(0);
-            } 
+//            if (gamepad1.a) { // Extend the slide
+//                hSlideMotor.runToPosition(280);
+//            } else if (gamepad1.b) { // Retract the slide
+//                hSlideMotor.runToPosition(0);
+//            }
 
-            if (gamepad1.y && slidestate == ExtensionMode.IDLE) { // Extend the slide
-                vSlideMotorOne.runToPosition(-1000);
-                vSlideMotorTwo.runToPosition(-1000);
-                if(!vSlideMotorOne.isBusy()) vSlideMotorOne.stop();
-                if(!vSlideMotorTwo.isBusy()) vSlideMotorTwo.stop();
-                slidestate = ExtensionMode.EXTENDED;
-            } else if (gamepad1.x && slidestate == ExtensionMode.EXTENDED) { // Retract the slide
-                vSlideMotorOne.setPowerWithoutPosition(1.0d);
-                vSlideMotorTwo.setPowerWithoutPosition(1.0d);
-                vSlideMotorOne.setBreak();
-                vSlideMotorTwo.setBreak();
-                slideTimer.reset();
-                slidestate = ExtensionMode.RETRACTED;
-            }
+//            if (gamepad1.y && slidestate == ExtensionMode.IDLE) { // Extend the slide
+//                vSlideMotorOne.runToPosition(-1000);
+//                vSlideMotorTwo.runToPosition(-1000);
+//                if(!vSlideMotorOne.isBusy()) vSlideMotorOne.stop();
+//                if(!vSlideMotorTwo.isBusy()) vSlideMotorTwo.stop();
+//                slidestate = ExtensionMode.EXTENDED;
+//            } else if (gamepad1.x && slidestate == ExtensionMode.EXTENDED) { // Retract the slide
+//                vSlideMotorOne.setPowerWithoutPosition(1.0d);
+//                vSlideMotorTwo.setPowerWithoutPosition(1.0d);
+//                vSlideMotorOne.setBreak();
+//                vSlideMotorTwo.setBreak();
+//                slideTimer.reset();
+//                slidestate = ExtensionMode.RETRACTED;
+//            }
+//
+//            if(vSlideMotorOne.getCurrent(CurrentUnit.AMPS) > 6.5 && slidestate == ExtensionMode.RETRACTED) {
+//                vSlideMotorOne.stopAndReset();
+//                vSlideMotorTwo.stopAndReset();
+//                vSlideMotorOne.setBreak();
+//                vSlideMotorTwo.setBreak();
+//                slidestate = ExtensionMode.IDLE;
+//            }
 
-            if(vSlideMotorOne.getCurrent(CurrentUnit.AMPS) > 6.5 && slidestate == ExtensionMode.RETRACTED) {
-                vSlideMotorOne.stopAndReset();
-                vSlideMotorTwo.stopAndReset();
-                vSlideMotorOne.setBreak();
-                vSlideMotorTwo.setBreak();
-                slidestate = ExtensionMode.IDLE;
-            }
-
-            if(gamepad1.dpad_right) {
-                //sequence.run("intakeNeutral");
-                sequence.run("intakeGrab")
+            if (gamepad1.dpad_right) {
+                if (currentTransferState == transferState.H_EXTENDED) {
+                    //sequence.run("intakeNeutral");
+                    sequence.run("intakeGrab");
+                    currentTransferState = transferState.H_INTAKEN;
                 }
-            if(gamepad1.dpad_left) {
-                sequence.run("transfer")
-                }
-            if(gamepad1.dpad_down) {
-                sequence.run("intakeNeutral")
-                    }
-            if(gamepad1.dpad_up){
-                outtakeClawServo.setposition(.4f)
-                }
-            if(gamepad2.dpad_up) {
-                Actions.runBlocking(
-                        drive.actionBuilder(beginPose)
-                                .splineTo(new Vector2d(30, 30), Math.PI / 2)
-                                .splineTo(new Vector2d(0, 60), Math.PI)
-                                .build());
             }
 
-            if(slideTimer.milliseconds() > 2000 && slidestate == ExtensionMode.RETRACTED) {
-                vSlideMotorOne.stopAndReset();
-                vSlideMotorTwo.stopAndReset();
-                vSlideMotorOne.setBreak();
-                vSlideMotorTwo.setBreak();
-                slidestate = ExtensionMode.IDLE;
+            if (gamepad1.dpad_down) {
+                if (currentTransferState == transferState.H_INTAKEN) {
+                    sequence.run("transfer");
+                    currentTransferState = transferState.TRANSFERED;
+                }
             }
+            if (gamepad1.dpad_left) {
+                if (currentTransferState == transferState.H_IDLE || currentTransferState == transferState.H_INTAKEN) {
+                    sequence.run("intakeNeutral");
+                    currentTransferState = transferState.H_EXTENDED;
+                }
+            }
+            if (gamepad1.dpad_up) {
+                if (currentTransferState == transferState.TRANSFERED) {
+                    outtakeClawServo.setPosition(.4f);
+                    currentTransferState = transferState.H_IDLE;
+                }
+            }
+            if (gamepad2.dpad_up) {
+                Actions.runBlocking(drive.actionBuilder(beginPose)
+                        .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                        .splineTo(new Vector2d(0, 60), Math.PI)
+                        .build());
+            }
+
+//            if(slideTimer.milliseconds() > 2000 && slidestate == ExtensionMode.RETRACTED) {
+//                vSlideMotorOne.stopAndReset();
+//                vSlideMotorTwo.stopAndReset();
+//                vSlideMotorOne.setBreak();
+//                vSlideMotorTwo.setBreak();
+//                slidestate = ExtensionMode.IDLE;
+//            }
 
             //mechanumDrive.fieldCentricDrive(x, y, rx);
 
             double newTime = getRuntime();
-            double loopTime = newTime-oldTime;
-            double frequency = 1/loopTime;
+            double loopTime = newTime - oldTime;
+            double frequency = 1 / loopTime;
             oldTime = newTime;
 
             // Show the elapsed game time and wheel power.
